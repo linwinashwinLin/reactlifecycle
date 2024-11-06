872b0f7535796dfcdbec39aa6eabc05d4f885d19
ashwin-14876 <ashwin.madhu@zohocorp.com>
2024-11-06 13:24:34 +0530

    centre

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <center>
+
     <App />
+     </center>
   </React.StrictMode>
 );
